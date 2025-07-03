3da9658007bd927ca33698634167729b2e305743
Nelson <nelsonsalto2@gmail.com>
2025-07-03 04:12:03 +0000

    Inicializacion de frontend con Vite y Tailwind configurado

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center p-10 text-2xl text-blue-600">
+      ¡Frontend funcionando con Tailwind!
+    </div>
+  );
+}
+
+export default App;
